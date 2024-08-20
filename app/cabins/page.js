4e4eb03abb63e7b919e701976a74c42d3b2a5231
Ala Ben Aissia <xalabenaissiax@outlook.com>
2024-08-20 14:40:15 +0100

    Data fetching in a Page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,16 @@
-export default function Page() {
-  return <h1>Cabins page</h1>
+export default async function Page() {
+  const users = await fetch(
+    'https://jsonplaceholder.typicode.com/users',
+  ).then((res) => res.json())
+
+  return (
+    <>
+      <h1>Cabins page</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
