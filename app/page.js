83c114d42d0f23bdad2b5984c3a5267e75a6e94e
Ala Ben Aissia <xalabenaissiax@outlook.com>
2024-08-22 15:24:34 +0100

    Building the cabin page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,29 @@
-export default function Home() {
-  return <h1>Home page</h1>
+import bg from '@/public/bg.png'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Page() {
+  return (
+    <div className='mt-24'>
+      <Image
+        src={bg}
+        alt='Mountains and forests with two cabins'
+        className='object-cover object-top'
+        fill
+        placeholder='blur'
+      />
+
+      <div className='relative z-10 text-center'>
+        <h1 className='text-8xl text-primary-50 mb-10 tracking-tight font-normal'>
+          Welcome to paradise.
+        </h1>
+        <Link
+          href='/cabins'
+          className='bg-accent-500 px-8 py-6 text-lg font-semibold hover:bg-accent-600 transition-all'
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </div>
+  )
 }
