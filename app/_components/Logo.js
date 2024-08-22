99c91ac606e6b1deb75dc8ba998766a3000989e9
Ala Ben Aissia <xalabenaissiax@outlook.com>
2024-08-22 14:33:31 +0100

    images optimization with Next.js Image component

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,14 +1,25 @@
+import logo from '@/public/logo.png'
+import Image from 'next/image'
 import Link from 'next/link'
 
 function Logo() {
   return (
     <Link href='/' className='flex items-center gap-4 z-10'>
-      <img
-        src='logo.png'
+      <Image
+        src={logo}
+        height='55'
+        width='55'
+        alt='The Wild Oasis logo'
+        quality={100}
+        priority
+      />
+      {/* <Image
+        src='/logo.png'
         height='60'
         width='60'
         alt='The Wild Oasis logo'
-      />
+        priority
+      /> */}
       <span className='text-xl font-semibold text-primary-100'>
         The Wild Oasis
       </span>
