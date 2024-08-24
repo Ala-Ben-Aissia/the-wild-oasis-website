d06919964da35ac87b616025d8c5b87a344687fd
Ala Ben Aissia <xalabenaissiax@outlook.com>
2024-08-24 18:31:34 +0100

    Dynamic route segments Building the Cabin Page

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -9,26 +9,35 @@ import Image from 'next/image'
 export default async function Page({params: {cabinId}}) {
   const cabin = await getCabin(cabinId)
 
+  const {
+    id,
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+    description,
+  } = cabin
+
   return (
     <div className='max-w-6xl mx-auto mt-8'>
       <div className='grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24'>
         <div className='relative scale-[1.15] -translate-x-3'>
           <Image
-            src={cabin.image}
-            alt={`Cabin ${cabin.name}`}
+            src={image}
+            alt={`Cabin ${name}`}
             fill
             className='object-cover'
-            sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
           />
         </div>
 
         <div>
           <h3 className='text-accent-100 font-black text-7xl mb-5 translate-x-[-254px] bg-primary-950 p-6 pb-1 w-[150%]'>
-            Cabin {cabin.name}
+            Cabin {name}
           </h3>
 
           <p className='text-lg text-primary-300 mb-10'>
-            {cabin.description}
+            {description}
           </p>
 
           <ul className='flex flex-col gap-4 mb-7'>
@@ -36,7 +45,7 @@ export default async function Page({params: {cabinId}}) {
               <UsersIcon className='h-5 w-5 text-primary-600' />
               <span className='text-lg'>
                 For up to{' '}
-                <span className='font-bold'>{cabin.maxCapacity}</span>{' '}
+                <span className='font-bold'>{maxCapacity}</span>{' '}
                 guests
               </span>
             </li>
