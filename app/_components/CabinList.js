e08c794efeea3c40d11bfa43fa8c3e55d954961d
Ala Ben Aissia <xalabenaissiax@outlook.com>
2024-08-24 18:20:27 +0100

    Dynamic route segments Building the Cabin Page

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -1,4 +1,4 @@
-import { getCabins } from '../_lib/data-service'
+import {getCabins} from '../_lib/data-service'
 import CabinCard from './CabinCard'
 
 export default async function CabinList() {
@@ -7,7 +7,7 @@ export default async function CabinList() {
   if (!cabins.length) return null
 
   return (
-    <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+    <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14'>
       {cabins.length > 0 &&
         cabins.map((cabin) => (
           <CabinCard cabin={cabin} key={cabin.id} />
--- app/cabins/loading.js
+++ app/cabins/loading.js
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className='grid justify-center items-center'>
       <Spinner />
-      <p>Loading cabins data...</p>
+      <p>Loading cabin data...</p>
     </div>
   )
 }
