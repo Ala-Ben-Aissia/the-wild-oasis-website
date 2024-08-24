9aec4254662f00f6b4f9cb0cfa384c8ead7e7cc1
Ala Ben Aissia <xalabenaissiax@outlook.com>
2024-08-24 09:46:03 +0100

    Fix Profile SideNavigation

--- app/layout.js
+++ app/layout.js
@@ -24,9 +24,9 @@ export default function RootLayout({children}) {
       >
         <Header />
         <div
-          className='flex-1 px-8 py-12' /* to vertically (flex-col) occupy the rest of the layout */
+          className='flex-1 px-8 py-12 grid' /* to vertically (flex-col) occupy the rest of the layout */
         >
-          <main className='max-w-7xl mx-auto'>{children}</main>
+          <main className='max-w-7xl mx-auto w-full'>{children}</main>
         </div>
       </body>
     </html>
