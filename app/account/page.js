bad3ebc8d24d70d53dd7e2371d72c516eabac074
Ala Ben Aissia <xalabenaissiax@outlook.com>
2024-09-04 19:17:26 +0100

    Building custom signIn page and signOut using server actions

--- app/account/page.js
+++ app/account/page.js
@@ -1,12 +1,16 @@
+import {auth} from '../_lib/auth'
+
 export const metadata = {
   title: 'Account',
 }
 
-export default function Page() {
+export default async function Page() {
+  const {user} = await auth()
+  // this is a protected route so user will  always be there
   return (
     <div>
       <h2 className='font-semibold text-2xl text-accent-400 mb-7'>
-        Welcome, Ala
+        Welcome, {user.name.split(' ').at(0)}
       </h2>
     </div>
   )
