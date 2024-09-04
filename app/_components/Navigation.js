57f54d251a6a23bb7ca86488f5f814e23f926eca
Ala Ben Aissia <xalabenaissiax@outlook.com>
2024-09-04 15:13:28 +0100

    getting the user session

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,10 @@
 import Link from 'next/link'
+import {auth} from '../_lib/auth'
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth() // auth implicitly uses cookies and headers which will cause switching the route from static to dynamic
+  // Since Navigation (dynamic) is use used everywhere (in the RootLayout), all SCs will also switch to dynamic which is not ideal (increases the load on the server)
+
   return (
     <nav className='z-10 text-xl'>
       <ul className='flex gap-16 items-center'>
@@ -21,12 +25,27 @@ export default function Navigation() {
           </Link>
         </li>
         <li>
-          <Link
-            href='/account'
-            className='hover:text-accent-400 transition-colors'
-          >
-            Guest area
-          </Link>
+          {session?.user ? (
+            <Link
+              href='/account'
+              className='hover:text-accent-400 transition-colors flex items-center gap-4'
+            >
+              <img
+                src={session.user.image}
+                alt={session.user.name}
+                className='h-10 rounded-full'
+                referrerPolicy='no-referrer' // necessary in some situations to correctly display images coming from Google
+              />
+              <span>Guest area</span>
+            </Link>
+          ) : (
+            <Link
+              href='/account'
+              className='hover:text-accent-400 transition-colors'
+            >
+              <span>Guest area</span>
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
